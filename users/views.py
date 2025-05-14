b35f29e984ddaf8640c3116c8ef5cbdebcb76bf1
khomyakov362 <khomyakov362@gmail.com>
2025-05-14 16:08:49 +0500

    CRUD written

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics  
+from rest_framework_simplejwt import views
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from users import serializers
+from users.models import User
+
+class ListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.Serializer
+
+class CreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.CreateSerializer
+    permission_classes = (AllowAny,)
+
+class RetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.Serializer
+    permission_classes = (AllowAny,)
+
+class UpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UpdateSerializer
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(id=user.id)
+
+class DestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+class TokenObtainPairView(views.TokenObtainPairView):
+    serializer_class = serializers.TokenObtainPairSerializer
